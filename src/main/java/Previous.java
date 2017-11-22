ea18294d146695d9e1a2dd2c59dc7fa7732afc4f
sergey-lagutin <sergey.v.lagutin@gmail.com>
2017-11-22 15:05:12 +0100

    Backup v2.

--- src/main/java/Previous.java
+++ src/main/java/Previous.java
@@ -2,6 +2,6 @@ import java.io.IOException;
 
 public class Previous {
     public static void main(String[] args) throws IOException {
-        new Runner(new MyStrategy_v1(), 31002).run();
+        new Runner(new MyStrategy_v2(), 31002).run();
     }
 }
