ee04ed845f2efac2887fd4515024acc7ee06d0bb
sergey-lagutin <sergey.v.lagutin@gmail.com>
2017-11-29 16:59:34 +0100

    Backup strategy.

--- src/main/java/Latest.java
+++ src/main/java/Latest.java
@@ -2,6 +2,6 @@ import java.io.IOException;
 
 public class Latest {
     public static void main(String[] args) throws IOException {
-        new Runner(new MyStrategy(), 31001).run();
+        new Runner(new MyStrategy(), 31002).run();
     }
 }
